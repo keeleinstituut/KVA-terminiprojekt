d78e53bd19ded97bb551349fdb92f17a90211f82
Sandra Eiche <sandraeiche7@gmail.com>
2024-06-19 11:02:14 +0300

    File_upload.py värskendus

--- app/views/file_upload.py
+++ app/views/file_upload.py
@@ -1,17 +1,14 @@
 import collections
 import param
 import panel as pn
-import os
-import sys
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 from app.controllers.pdf_upload_controller import upload_to_db
-from utils.upload_helpers import normalized_keywords
-
-
+from utils.upload_helpers import normalized_input_lists
+import time
+import copy
 
 BUTTON_WIDTH = 125
+INPUT_WIDTH = 300
 
 USER_INPUT_FIELDS = collections.OrderedDict(
     {"publication": pn.widgets.TextInput(name='Publikatsioon'),
@@ -20,7 +17,7 @@ USER_INPUT_FIELDS = collections.OrderedDict(
      "author": pn.widgets.TextInput(name='Autor'),
      "languages": pn.widgets.TextInput(name='Keeled', description='Eralda keeled komadega'),
      "keywords": pn.widgets.TextInput(name='Märksõnad', description='Eralda märksõnad komadega'),
-     "is_valid": pn.widgets.Checkbox(name='Kehtetu')}
+     "is_valid": pn.widgets.Checkbox(name='Kehtetu', value=False)}
 )
 
 DEFAULT_METADATA = {"publication": "",
@@ -29,7 +26,7 @@ DEFAULT_METADATA = {"publication": "",
               "author": "",
               "languages": [],
               "keywords": [],
-              "is_valid": True}
+              "is_valid": False}
 
 class DataHandler(param.Parameterized):
 
@@ -39,13 +36,16 @@ class DataHandler(param.Parameterized):
     user_input_widgets = collections.OrderedDict()
 
     # PDF browsing
-    pdf_input = pn.widgets.FileInput(accept='.pdf', name='Vali fail', filename='')
+    pdf_input = pn.widgets.FileInput(accept='.pdf', name='Vali fail', filename='', width=INPUT_WIDTH)
     pdf_viewer = pn.pane.PDF(pdf_input, width=500, height=800)
 
     submit_button = pn.widgets.Button(button_type='primary', name='Salvesta', align='start', width=BUTTON_WIDTH)
     new_entry_button = pn.widgets.Button(button_type='primary', name='Uus fail', align='end', width=BUTTON_WIDTH)
 
-    metadata = DEFAULT_METADATA
+    static_text = pn.widgets.StaticText(name='Static Text', value='A string')
+    alert_pane = pn.pane.Alert(object = 'Sisesta fail!', alert_type='warning', width = INPUT_WIDTH, height = 70, visible = False)
+
+    metadata = copy.deepcopy(DEFAULT_METADATA)
 
     def __init__(self,**params):
         super().__init__(**params)
@@ -55,28 +55,39 @@ class DataHandler(param.Parameterized):
 
     @param.depends('submit_button.value')
     def refresh_metadata(self):
+        self.submit_button.disabled=True
+        self.new_entry_button.disabled=True
+        time.sleep(3)
         for name, widget in self.user_input_widgets.items():
+            field_input = widget.value
             if name == 'is_valid':
-                self.metadata[name] = not widget.value
-            if name in ['keywords', 'languages']:
-                self.metadata[name] = normalized_keywords(widget.value)
-            if name == 'filename':
+                self.metadata[name] = not field_input
+            elif name in ['keywords', 'languages']:
+                self.metadata[name] = normalized_input_lists(widget.value)
+            elif name == 'filename':
                 self.metadata[name] = widget.placeholder
             elif isinstance(widget.value, str):
-                self.metadata[name] = widget.value.strip()
+                self.metadata[name] = field_input.strip()
             else:
-                self.metadata[name] = widget.value
-        
+                self.metadata[name] = field_input
+
         if self.pdf_input.value:
-            print('Loading:\n', self.metadata)
-            self.submit_button.disabled=True
-            upload_to_db(self.pdf_input.value, self.metadata)
+            status = upload_to_db(self.pdf_input.value, self.metadata)
+            if status == -1:
+                self.alert_pane.param.update(object = 'Sellise pealkirjaga fail on juba baasis olemas!', visible=True)
+        else:
+            self.alert_pane.param.update(visible=True)
+        
+        self.new_entry_button.disabled=False
+            
             
     @param.depends('new_entry_button.value')
     def new_input(self):
         """
         Resetting fields to upload a new file.
         """
+        # Todo: uue saab alles siis sisestada, kui json on valmis ja metaandmed postgres? 
+        self.alert_pane.param.update(visible=False)
         for name, widget in self.user_input_widgets.items():
             if isinstance(widget, pn.widgets.TextInput):
                 widget.value = ""
@@ -86,7 +97,7 @@ class DataHandler(param.Parameterized):
             self.pdf_input.value = None
             self.pdf_input.filename = ''
 
-        self.metadata = DEFAULT_METADATA
+        self.metadata = copy.deepcopy(DEFAULT_METADATA)
         self.submit_button.disabled=False
 
 def file_upload():
@@ -97,7 +108,8 @@ def file_upload():
     left_column = pn.Column(metadata.pdf_input,
                             *metadata.user_input_widgets.values(),
                             pn.Row(metadata.submit_button, metadata.refresh_metadata, 
-                                    metadata.new_entry_button, metadata.new_input)
+                                    metadata.new_entry_button, metadata.new_input),
+                            metadata.alert_pane
                             )
 
     page_layout = pn.Row(left_column, right_column)
