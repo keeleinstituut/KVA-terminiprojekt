b13815ce184d018dc224090e1f51e6ae42fd362b
Sandra Eiche <sandraeiche7@gmail.com>
2024-07-01 11:14:45 +0300

    Action handling refakto

--- app/views/file_upload.py
+++ app/views/file_upload.py
@@ -29,10 +29,20 @@ DEFAULT_METADATA = {"publication": "",
               "is_valid": False}
 
 class DataHandler(param.Parameterized):
+    """
+    A class that handles user input for uploading PDF files and associated metadata.
 
-    action = param.Action(
-        default=lambda x: x.param.trigger('action'), label='Salvesta')
-
+    Attributes:
+        user_input_widgets (OrderedDict): A dictionary containing all of the user input widgets for metadata fields.
+        pdf_input (pn.widgets.FileInput): A file input widget for selecting a PDF file.
+        pdf_viewer (pn.pane.PDF): A panel for displaying the selected PDF file.
+        submit_button (pn.widgets.Button): A button for submitting the user input and uploading the file.
+        new_entry_button (pn.widgets.Button): A button for resetting the input fields to upload a new file.
+        static_text (pn.widgets.StaticText): A static text widget.
+        alert_pane (pn.pane.Alert): An alert pane for displaying warning messages.
+        metadata (dict): A dictionary containing the metadata for the uploaded file.
+    """
+    
     user_input_widgets = collections.OrderedDict()
 
     # PDF browsing
@@ -41,7 +51,7 @@ class DataHandler(param.Parameterized):
 
     submit_button = pn.widgets.Button(button_type='primary', name='Salvesta', align='start', width=BUTTON_WIDTH)
     new_entry_button = pn.widgets.Button(button_type='primary', name='Uus fail', align='end', width=BUTTON_WIDTH)
-
+ 
     static_text = pn.widgets.StaticText(name='Static Text', value='A string')
     alert_pane = pn.pane.Alert(object = 'Sisesta fail!', alert_type='warning', width = INPUT_WIDTH, height = 70, visible = False)
 
@@ -50,19 +60,21 @@ class DataHandler(param.Parameterized):
     def __init__(self,**params):
         super().__init__(**params)
         self.user_input_widgets['filename'] = pn.widgets.TextInput(name='Faili pealkiri', placeholder=self.pdf_input.param.filename, disabled=True)
-        self.user_input_widgets.update({meta_field: widget 
-                                for meta_field, widget in USER_INPUT_FIELDS.items()})
+        self.user_input_widgets.update(USER_INPUT_FIELDS)
 
-    @param.depends('submit_button.value')
-    def refresh_metadata(self):
+    @param.depends('submit_button.value', watch=True)
+    def refresh_metadata(self) -> None:
+        """ Updates the metadata dictionary with the user input values and uploads the metadata to the database. """
         self.submit_button.disabled=True
         self.new_entry_button.disabled=True
-        time.sleep(3)
+        time.sleep(5)
         for name, widget in self.user_input_widgets.items():
             field_input = widget.value
             if name == 'is_valid':
                 self.metadata[name] = not field_input
-            elif name in ['keywords', 'languages']:
+            elif name == 'keywords':
+                self.metadata[name] = normalized_input_lists(widget.value_input)
+            elif name == 'languages':
                 self.metadata[name] = normalized_input_lists(widget.value)
             elif name == 'filename':
                 self.metadata[name] = widget.placeholder
@@ -81,34 +93,41 @@ class DataHandler(param.Parameterized):
         self.new_entry_button.disabled=False
             
             
-    @param.depends('new_entry_button.value')
-    def new_input(self):
+    @param.depends('new_entry_button.value', watch=True)
+    def new_input(self) -> None:
         """
-        Resetting fields to upload a new file.
+        Resets the input fields to their default values for uploading a new file.
         """
         # Todo: uue saab alles siis sisestada, kui json on valmis ja metaandmed postgres? 
         self.alert_pane.param.update(visible=False)
         for name, widget in self.user_input_widgets.items():
             if isinstance(widget, pn.widgets.TextInput):
                 widget.value = ""
+                widget.value_input = ""
             else:
                 widget.value = DEFAULT_METADATA[name]
-        if self.pdf_input.value:
-            self.pdf_input.value = None
-            self.pdf_input.filename = ''
+        if self.pdf_input.param.value:
+            self.pdf_input.param.update(value = None, filename= '')
 
         self.metadata = copy.deepcopy(DEFAULT_METADATA)
         self.submit_button.disabled=False
 
-def file_upload():
+def file_upload() -> pn.layout.ListPanel:
+    """
+    Creates and returns the layout of the user interface for file upload page.
+
+    Returns:
+        pn.Row: A row layout containing the left column (file input, metadata input fields, buttons, and alert pane)
+                and the right column (PDF viewer).
+    """
 
     metadata = DataHandler()
 
     right_column = pn.Column(metadata.pdf_viewer)
     left_column = pn.Column(metadata.pdf_input,
                             *metadata.user_input_widgets.values(),
-                            pn.Row(metadata.submit_button, metadata.refresh_metadata, 
-                                    metadata.new_entry_button, metadata.new_input),
+                            pn.Row(metadata.submit_button,
+                                    metadata.new_entry_button),
                             metadata.alert_pane
                             )
 
