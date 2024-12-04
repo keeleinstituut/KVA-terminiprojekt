d1e77fc16567d22f7a160cd463fd40a8a8970730
Sandra Eiche <sandraeiche7@gmail.com>
2024-12-04 11:49:34 +0200

    QdrantChat nimetamine Retriever'iks; reformat ja parandused dokumentatsioonis

--- app/models/chatter_model.py
+++ app/models/chatter_model.py
@@ -3,20 +3,19 @@ import logging
 from collections import defaultdict
 
 from qdrant_client import QdrantClient
-from qdrant_client.http.models import (FieldCondition, Filter, MatchAny,
-                                       MatchValue)
+from qdrant_client.http.models import FieldCondition, Filter, MatchAny, MatchValue
 from sentence_transformers import SentenceTransformer
 import re
 from langchain_anthropic import ChatAnthropic
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
-# import weave
 
 # Configure logging
-logger = logging.getLogger('app')
+logger = logging.getLogger("app")
 logger.setLevel(logging.INFO)
 
-class FilterFactory():
+
+class FilterFactory:
     """
     A class for creating and managing filters for the Qdrant search engine.
 
@@ -42,47 +41,51 @@ class FilterFactory():
     response_limit = 5
 
     chunk_validity_filter = FieldCondition(
-            key="validated",
-            match=MatchValue(
-                value=True,
-            ),)
-    
-    document_validity_filter = FieldCondition(
-    key = "is_valid",
-    match=MatchValue(value=True,)
+        key="validated",
+        match=MatchValue(
+            value=True,
+        ),
     )
-    
+
+    document_validity_filter = FieldCondition(
+        key="is_valid",
+        match=MatchValue(
+            value=True,
+        ),
+    )
+
     file_filter = FieldCondition(
         key="title",
-        match=MatchAny(any=target_files),)
+        match=MatchAny(any=target_files),
+    )
 
-
-    def apply_filters(self, files: list, response_limit: int, document_validity: bool) -> None:
-        """ Coordinates modfication of filter settings. """
+    def apply_filters(
+        self, files: list, response_limit: int, document_validity: bool
+    ) -> None:
+        """Coordinates modfication of filter settings."""
         self.update_file_filter_target_files(files)
         self.set_response_limit(response_limit)
         self.set_document_validity(document_validity)
-    
+
     def update_file_filter_target_files(self, files: list) -> None:
-        """ Updates the target files for the file filter. """
+        """Updates the target files for the file filter."""
         self.target_files = files
         if files:
             self.file_filter.match = MatchAny(any=self.target_files)
             self.apply_file_filter = True
         else:
             self.apply_file_filter = False
-    
+
     def set_response_limit(self, response_limit: int) -> None:
-        """ Sets the maximum number of search results to return. """
+        """Sets the maximum number of search results to return."""
         self.response_limit = response_limit
 
     def set_document_validity(self, document_validity: bool) -> None:
-        """ Sets whether to apply the document validity filter. """
+        """Sets whether to apply the document validity filter."""
         self.apply_document_validity_filter = document_validity
 
-    # @weave.op()
     def assemble_filter(self) -> Filter:
-        """ Assembles and returns the final filter based on the current settings. """
+        """Assembles and returns the final filter based on the current settings."""
         conditions = []
         if self.apply_file_filter:
             conditions.append(self.file_filter)
@@ -94,7 +97,7 @@ class FilterFactory():
         return Filter(must=conditions)
 
 
-class QdrantChat():
+class Retriever:
     """
     A class for interacting with a Qdrant vector search engine to retrieve relevant responses based on user input.
 
@@ -107,35 +110,44 @@ class QdrantChat():
         port (int): The port number of the Qdrant server.
         client (QdrantClient): The Qdrant client instance for interacting with the server.
     """
-    
-    def __init__(self, model_name: str, collection_name: str, filterfactory: FilterFactory, prompt: str) -> None:
+
+    def __init__(
+        self,
+        model_name: str,
+        collection_name: str,
+        filterfactory: FilterFactory,
+        prompt: str,
+    ) -> None:
         self.model = SentenceTransformer(model_name)
         self.collection_name = collection_name
         self.filterfactory = filterfactory
         self.prompt = prompt
 
     def connect(self, host: str, port: int) -> None:
-        """  Connects to the Qdrant server using the provided host and port. """
+        """Connects to the Qdrant server using the provided host and port."""
         self.host = host
         self.port = port
         self.client = QdrantClient(host, port=port)
 
-    async def chat_callback(self, contents: str, user, instance) -> str:
-        """ A callback function for handling user input and generating responses. """
-        await asyncio.sleep(1.8)
+    async def retrieve_context(self, contents: str, user, instance) -> str:
+        """A callback function for handling user input and generating LLM context information based on retrieved documents."""
+        await asyncio.sleep(0.5)
         db_filter = self.filterfactory.assemble_filter()
-        logger.info('Filter assembled')
+        logger.info("Filter assembled")
 
         # Get similar documents
         results = self.get_similarities(self.prompt + contents, db_filter)
 
         # Assemble response string
-        response = ''
-        for result in zip(results['response_text'], results['title'], results['page_no']):
-            response += f'Title: {result[1]}\nPage: {result[2]}\n\n{result[0]}\n{"*"*15}\n'
+        response = ""
+        for result in zip(
+            results["response_text"], results["title"], results["page_no"]
+        ):
+            response += (
+                f'Title: {result[1]}\nPage: {result[2]}\n\n{result[0]}\n{"*"*15}\n'
+            )
 
         return response
-
 
     def get_similarities(self, text: str, query_filter: Filter) -> dict:
         """
@@ -149,36 +161,52 @@ class QdrantChat():
             dict: A dictionary containing lists of response texts, response types, scores, filenames, and page numbers.
         """
 
-        logger.info('Searching for similar documents')
+        logger.info("Searching for similar documents")
         search_result = self.client.search(
             collection_name=self.collection_name,
-            query_vector=list(self.model.encode(text, normalize_embeddings=True).astype(float)),
+            query_vector=list(
+                self.model.encode(text, normalize_embeddings=True).astype(float)
+            ),
             query_filter=query_filter,
             limit=self.filterfactory.response_limit,
-            timeout=100)
-        logger.info('Search results retieved')
-        
+            timeout=100,
+        )
+        logger.info("Search results retieved")
+
         result_dict = defaultdict(list)
-        
+
         for point in search_result:
             if not point.payload:
                 continue
-            result_dict['response_text'].append(point.payload["text"])
-            result_dict['response_type'].append(point.payload["content_type"])
-            result_dict['score'].append(point.score)
-            result_dict['filename'].append(point.payload["filename"])
-            result_dict['page_no'].append(point.payload["page_number"])
-            result_dict['title'].append(point.payload["title"])
+            result_dict["response_text"].append(point.payload["text"])
+            result_dict["response_type"].append(point.payload["content_type"])
+            result_dict["score"].append(point.score)
+            result_dict["filename"].append(point.payload["filename"])
+            result_dict["page_no"].append(point.payload["page_number"])
+            result_dict["title"].append(point.payload["title"])
 
         return result_dict
-    
 
-class LLMChat():
-    
+
+class LLMChat:
+    """
+    A class for interacting with large language models (LLMs) such as OpenAI's GPT or Anthropic's Claude.
+
+    Attributes:
+        model_name (str): The name of the LLM model to use (e.g., "gpt-3.5", "claude-3.5-sonnet-latest").
+        retriever (Retriever): A retriever instance for fetching contextual data based on user input.
+        api_key (str): The API key for authenticating requests to the LLM provider.
+        chat_template (ChatPromptTemplate): A predefined template for structuring chat messages sent to the LLM.
+        temperature (float): Controls the randomness of the model's responses (default is 0).
+        max_retries (int): The maximum number of retry attempts for failed API calls (default is 2).
+        llm (ChatAnthropic or ChatOpenAI): The connected LLM client instance for generating responses.
+    """
+
     chat_template = ChatPromptTemplate.from_messages(
-    [
-        ("system", 
-        """
+        [
+            (
+                "system",
+                """
 **Role:** You are a terminologist searching for terminological information about a keyword. 
  
 **Objective:** You've collected key sections from various documents about the keyword. Your task is to analyze these sections in order to create a comprehensive term entry. ALWAYS use EXACT QUOTES unless otherwise stated. 
@@ -219,14 +247,21 @@ class LLMChat():
 	           1. Contextual paragraph text (Document title, Page no)
 	 
     **See Also:**  [Insert terms, abbreviations, or synonyms, separate terms using semicolons]
-        """),
-        ("human", "Keyword: {user_input}"),
-        ("human", "Key sections:\n{retrieval_results}")
-    ]
+        """,
+            ),
+            ("human", "Keyword: {user_input}"),
+            ("human", "Key sections:\n{retrieval_results}"),
+        ]
     )
 
-    def __init__(self, model_name: str, qdrant_chatter: QdrantChat, api_key, chat_template=chat_template) -> None:
-        self.qdrant_chatter = qdrant_chatter
+    def __init__(
+        self,
+        model_name: str,
+        retriever: Retriever,
+        api_key,
+        chat_template=chat_template,
+    ) -> None:
+        self.retriever = retriever
         self.model_name = model_name
         self.temperature = 0
         self.max_retries = 2
@@ -235,36 +270,39 @@ class LLMChat():
         self.chat_template = chat_template
 
     def connect_language_model(self):
-
-        if re.match('claude', self.model_name):
+        if re.match("claude", self.model_name):
             llm = ChatAnthropic(
                 model_name=self.model_name,
                 temperature=self.temperature,
                 stop=None,
-                base_url='',
-                api_key=None,
+                base_url="https://api.anthropic.com",
+                api_key=self.api_key,
                 timeout=None,
                 max_retries=self.max_retries,
-                )
-            
-        elif re.match('gpt', self.model_name):
+            )
+
+        elif re.match("gpt", self.model_name):
             llm = ChatOpenAI(
                 model=self.model_name,
                 temperature=self.temperature,
                 max_tokens=None,
                 timeout=None,
                 max_retries=self.max_retries,
-                api_key=self.api_key
-                )
-            
+                api_key=self.api_key,
+            )
+
         return llm
-        
-    # @weave.op()
-    async def chat_callback(self, contents: str,):
-        """ A callback function for handling user input and generating responses. """
-        await asyncio.sleep(1.8)
-        context = await self.qdrant_chatter.chat_callback(contents, '', '')
-        prompt = self.chat_template.format_messages(user_input=contents, retrieval_results=context)
-        logger.info('Starting LLM query.')
+
+    async def chat_callback(
+        self,
+        contents: str,
+    ):
+        """A callback function for handling user input and generating responses."""
+        await asyncio.sleep(0.5)
+        context = await self.retriever.retrieve_context(contents, "", "")
+        prompt = self.chat_template.format_messages(
+            user_input=contents, retrieval_results=context
+        )
+        logger.info("Starting LLM query.")
         response = self.llm.invoke(prompt)
-        return response.content   
+        return response.content
--- app/views/llm_view.py
+++ app/views/llm_view.py
@@ -8,22 +8,23 @@ from panel.chat import ChatInterface, ChatMessage
 import pandas as pd
 
 from app.config import config
-from app.models.chatter_model import FilterFactory, QdrantChat, LLMChat
+from app.models.chatter_model import FilterFactory, Retriever, LLMChat
 from utils.db_connection import Connection
 
 import os
 
 pn.extension("perspective")
 
-CONNECTION_PARAMS = {'host': os.getenv('PG_HOST'), 
-                     'port': os.getenv('PG_PORT'), 
-                     'user': os.getenv('PG_USER'), 
-                     'password': os.getenv('PG_PASSWORD'), 
-                     'db': os.getenv('PG_COLLECTION'), 
-                     }
+CONNECTION_PARAMS = {
+    "host": os.getenv("PG_HOST"),
+    "port": os.getenv("PG_PORT"),
+    "user": os.getenv("PG_USER"),
+    "password": os.getenv("PG_PASSWORD"),
+    "db": os.getenv("PG_COLLECTION"),
+}
 
 # Configure logging
-logger = logging.getLogger('app')
+logger = logging.getLogger("app")
 logger.setLevel(logging.INFO)
 
 
@@ -40,11 +41,11 @@ class FilterActionHandler(param.Parameterized):
         file_selector (pn.widgets.CrossSelector): A widget to select document titles for filtering.
         limit_slider (pn.widgets.EditableIntSlider): A slider widget to set the limit for the
             number of responses.
-        validity_checkbox (pn.widgets.Checkbox): A checkbox widget to filter for documents marked as 
+        validity_checkbox (pn.widgets.Checkbox): A checkbox widget to filter for documents marked as
             valid by the user previously.
         filterfactory (FilterFactory): An instance of the FilterFactory class used for applying
             filters to the chat model.
-        con (Connection): An instance of the Connection class for interacting with the database.
+        con (Connection): An instance of the Connection class for interacting with Pg database.
         files_df (pd.DataFrame): A DataFrame containing the ids and titles of documents that have finished processing (included in vector database).
         keywords_df (pd.DataFrame): A DataFrame containing the list of keywords.
     """
@@ -56,22 +57,39 @@ class FilterActionHandler(param.Parameterized):
         self.con.establish_connection()
 
         self.apply_filters_button = pn.widgets.Button(
-        name='Rakenda filtrid', button_type='primary', width=50, margin=(20, 60, 0, 0))
-    
-        self.refresh_choices_button = pn.widgets.Button(
-            name='Värskenda filtrid', button_type='primary', width=50, margin=(20, 0, 0, 20))
+            name="Rakenda filtrid",
+            button_type="primary",
+            width=50,
+            margin=(20, 60, 0, 0),
+        )
 
-        self.keyword_selector = pn.widgets.CrossSelector(name='Märksõnad', value=[],
-                                                    options=[], size=8, width=500)
-        self.file_selector = pn.widgets.CrossSelector(name='Dokumendid', value=[],
-                                                options=[], size=8, width=500)
+        self.refresh_choices_button = pn.widgets.Button(
+            name="Värskenda filtrid",
+            button_type="primary",
+            width=50,
+            margin=(20, 0, 0, 20),
+        )
+
+        self.keyword_selector = pn.widgets.CrossSelector(
+            name="Märksõnad", value=[], options=[], size=8, width=500
+        )
+        self.file_selector = pn.widgets.CrossSelector(
+            name="Dokumendid", value=[], options=[], size=8, width=500
+        )
 
         self.limit_slider = pn.widgets.EditableIntSlider(
-            name='Tekstilõikude arv SKMi sisendis', start=1, end=20, step=1, value=5, width=500)
+            name="Tekstilõikude arv SKMi sisendis",
+            start=1,
+            end=20,
+            step=1,
+            value=5,
+            width=500,
+        )
 
         self.validity_checkbox = pn.widgets.Checkbox(
-            name='Otsi ainult kehtivatest', width=500)
-        
+            name="Otsi ainult kehtivatest", width=500
+        )
+
         super().__init__(**params)
 
         self.refresh_selectors()
@@ -80,54 +98,59 @@ class FilterActionHandler(param.Parameterized):
         """ Loads the list of keywords from the database. """
         try:
             keywords_df = self.con.statement_to_df(
-                """ SELECT DISTINCT keyword FROM keywords ORDER BY keyword""")
+                """ SELECT DISTINCT keyword FROM keywords ORDER BY keyword"""
+            )
             return keywords_df
         except Exception as e:
             logger.error(e)
-            return pd.DataFrame(columns=['keyword'])
+            return pd.DataFrame(columns=["keyword"])
 
     def load_files_from_db(self):
         """ Loads the list of processed document titles and ids from the database. """
         try:
             files_df = self.con.statement_to_df(
-                """ SELECT id, title FROM documents WHERE current_state = 'uploaded' ORDER BY title""")
+                """ SELECT id, title FROM documents WHERE current_state = 'uploaded' ORDER BY title"""
+            )
             return files_df
         except Exception as e:
             logger.error(e)
-            return pd.DataFrame(columns=['id', 'title'])
+            return pd.DataFrame(columns=["id", "title"])
 
-    @param.depends('refresh_choices_button.value', watch=True)
+    @param.depends("refresh_choices_button.value", watch=True)
     def refresh_selectors(self):
-        """ Refreshes the options for the keyword and document title selectors by utilizing previous methods. 
+        """ Refreshes the options for the keyword and document title selectors by utilizing previous methods.
         Responds to clicking refresh_choices_button. """
-        logger.info(
-            'Refreshing file selection. Established Postgres connection')
+        logger.info("Refreshing file selection. Established Postgres connection")
 
-        # Refreshing file list
         try:
             self.files_df = self.load_files_from_db()
-            self.file_selector.options = list(self.files_df['title'])
+            self.file_selector.options = list(self.files_df["title"])
             self.keywords_df = self.load_keywords_from_db()
-            self.keyword_selector.options = list(self.keywords_df['keyword'])
-            logger.info('File selection refresh complete.')
+            self.keyword_selector.options = list(self.keywords_df["keyword"])
+            logger.info("File selection refresh complete.")
 
         except Exception as e:
             logger.error(e)
             pass
 
-    @param.depends('keyword_selector.value', watch=True)
+    @param.depends("keyword_selector.value", watch=True)
     def keyword_filtering(self):
-        """ Filters the document title options based on the selected keywords. Responds to changes in keyword selector widget."""
+        """ Filters the document title options based on the selected keywords. Responds to changes in keyword selector widget. """
         selected_kw_values = self.keyword_selector.value
         if selected_kw_values:
             logger.info(selected_kw_values)
             self.con.establish_connection()
             try:
-                result = self.con.execute_sql("""SELECT document_id FROM keywords WHERE keyword IN :kws""", [
-                                              {'kws': (tuple(selected_kw_values))}])
-                compatible_document_ids = [row[0] for row in result['data']]
+                result = self.con.execute_sql(
+                    """SELECT document_id FROM keywords WHERE keyword IN :kws""",
+                    [{"kws": (tuple(selected_kw_values))}],
+                )
+                compatible_document_ids = [row[0] for row in result["data"]]
                 selected_files = list(
-                    self.files_df[self.files_df['id'].isin(compatible_document_ids)]['title'])
+                    self.files_df[self.files_df["id"].isin(compatible_document_ids)][
+                        "title"
+                    ]
+                )
                 self.file_selector.options = selected_files
                 self.file_selector.value = selected_files
             except Exception as e:
@@ -135,36 +158,43 @@ class FilterActionHandler(param.Parameterized):
         else:
             self.refresh_selectors()
 
-    @param.depends('apply_filters_button.value', watch=True)
+    @param.depends("apply_filters_button.value", watch=True)
     def apply_filters(self):
-        """ Applies the selected filters to the chat model using the FilterFactory. Responds to apply_filters_button widget."""
-        self.filterfactory.apply_filters(files=self.file_selector.value,
-                                         response_limit=self.limit_slider.value,
-                                         document_validity=self.validity_checkbox.value)
+        """ Applies the selected filters to the chat model using the FilterFactory. Responds to apply_filters_button widget. """
+        self.filterfactory.apply_filters(
+            files=self.file_selector.value,
+            response_limit=self.limit_slider.value,
+            document_validity=self.validity_checkbox.value,
+        )
 
 
 def llm_view():
-
-    client_host = os.getenv('QDRANT_HOST')
-    client_port = os.getenv('QDRANT_PORT')
+    client_host = os.getenv("QDRANT_HOST")
+    client_port = os.getenv("QDRANT_PORT")
 
     embedding_model = config["embeddings"]["embedding_model"]
-    collection_test = os.getenv('QDRANT_COLLECTION')
+    collection_test = os.getenv("QDRANT_COLLECTION")
 
-    api_key = os.getenv('LLM_API_KEY')
+    api_key = os.getenv("LLM_API_KEY")
     llm_model = config["llm"]["model"]
 
     filterfactory = FilterFactory()
-    chatter = QdrantChat(embedding_model, collection_test,
-                         filterfactory, prompt=config['embeddings']["query_prompt"])
-    llm_chatter = LLMChat(llm_model, chatter, api_key)
+    retriever = Retriever(
+        embedding_model,
+        collection_test,
+        filterfactory,
+        prompt=config["embeddings"]["query_prompt"],
+    )
+    llm_chatter = LLMChat(llm_model, retriever, api_key)
 
-    chatter.connect(client_host, client_port)
-    logger.info('Established Qdrant connection')
+    retriever.connect(client_host, client_port)
+    logger.info("Established Qdrant connection")
 
-    filter_handler = FilterActionHandler(chatter.filterfactory)
+    filter_handler = FilterActionHandler(retriever.filterfactory)
 
-    toggle = pn.widgets.ToggleIcon(icon='adjustments', active_icon='adjustments-off', size='4em', align='end')
+    toggle = pn.widgets.ToggleIcon(
+        icon="adjustments", active_icon="adjustments-off", size="4em", align="end"
+    )
 
     # Callback to show/hide the filters
     @param.depends(toggle.param.value, watch=True)
@@ -174,55 +204,62 @@ def llm_view():
         else:
             filter_column.visible = False
 
-
     text_area_input = pn.widgets.TextAreaInput(
         placeholder="Otsi dokumendist", auto_grow=True, max_rows=1
     )
 
+    # Handling sending input and response on Enter press
     def text_area_event_handler(event):
-        if event.new.endswith('\n'):  # Check if enter key was pressed
+        if event.new.endswith("\n"):  # Check if enter key was pressed
             asyncio.create_task(answer(text_area_input.value_input, text_area_input))
 
-
     async def answer(contents, active_widget):
-        contents = contents.strip('\n')
+        contents = contents.strip("\n")
         active_widget.param.update({"value": "", "value_input": ""})
         # Empty input field
-        ci.send(ChatMessage(contents, user="Terminoloog", show_reaction_icons=False, show_copy_icon=False), 
-                callback=True)
+        ci.send(
+            ChatMessage(
+                contents,
+                user="Terminoloog",
+                show_reaction_icons=False,
+                show_copy_icon=False,
+            ),
+            callback=True,
+        )
 
+    text_area_input.param.watch(text_area_event_handler, "value_input")
+
+    # Interface assembly
     ci = ChatInterface(
-        callback_exception='verbose',
+        callback_exception="verbose",
         widgets=text_area_input,
         user="Terminoloog",
         show_send=True,
         show_button_name=False,
         callback=llm_chatter.chat_callback,
-        callback_user='Assistent',
+        callback_user="Assistent",
         reset_on_send=True,
         show_stop=False,
         show_rerun=False,
         show_undo=False,
         show_copy_icon=False,
         sizing_mode="stretch_width",
-        reaction_icons = {})
- 
-     # Attach handler to the TextAreaInput's value
-    text_area_input.param.watch(text_area_event_handler, 'value_input')
-        
+        reaction_icons={},
+    )
 
     try:
-
         filter_column = pn.Column(
-            pn.pane.HTML('<label>Vali märksõnad</label>'),
+            pn.pane.HTML("<label>Vali märksõnad</label>"),
             filter_handler.keyword_selector,
-            pn.pane.HTML('<label>Vali dokumendid</label>'),
+            pn.pane.HTML("<label>Vali dokumendid</label>"),
             filter_handler.file_selector,
             filter_handler.limit_slider,
             filter_handler.validity_checkbox,
-            pn.Row(filter_handler.apply_filters_button, 
-                   filter_handler.refresh_choices_button),
-            visible=False
+            pn.Row(
+                filter_handler.apply_filters_button,
+                filter_handler.refresh_choices_button,
+            ),
+            visible=False,
         )
 
     except Exception as e:
